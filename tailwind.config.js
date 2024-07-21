66509105787e2ef70b951ba80000c1fde3cb5181
ganesh-3um <ganesh.s@3umgroup.com>
2024-07-20 22:13:17 -0400

    Added daisyui theme to storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,20 @@ module.exports = {
     "./.storybook/**/*.{js,jsx,ts,tsx}",
   ],
   // Toggle dark-mode based on .dark class or data-mode="dark"
-  darkMode: ['class', '[data-mode="dark"]'],
+  darkMode: ['class', '[data-mode="light"]'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  // Daisy UI configuration
+  daisyui: {
+    themes: ["light", "dark"], // Add the themes you want to use
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
