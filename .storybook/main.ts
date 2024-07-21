cf007aaaed72f75a7c3e84afb142fc06ce28edc8
ganesh-3um <ganesh.s@3umgroup.com>
2024-07-20 21:57:05 -0400

    re-configured the storybook settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,45 +1,49 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
 import path from 'path';
-
+import type { StorybookConfig } from "@storybook/react-webpack5";
+import { RuleSetRule } from 'webpack';
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-essentials",
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@storybook/addon-interactions",
+    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    // Additional addons as needed
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        postCss: true,
+      },
+    },
+    "@storybook/addon-themes"
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
   webpackFinal: async (config) => {
-    if (config.module) {
-      const rules = config.module.rules || [];
-
-      rules.push({
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  require('tailwindcss'),
-                  require('autoprefixer'),
-                ],
-              },
-            },
-          },
-        ],
-        include: path.resolve(__dirname, '../src'),
+    if (config.module?.rules) {
+      // Find and remove the existing rule for CSS files
+      const cssRuleIndex = config.module.rules.findIndex((rule): rule is RuleSetRule => {
+        if (typeof rule === 'string') return false;
+        if ('test' in rule && rule.test instanceof RegExp) {
+          return rule.test.test('.css');
+        }
+        return false;
       });
-      config.module.rules = rules;
-    }
 
+      if (cssRuleIndex !== -1) {
+        config.module.rules.splice(cssRuleIndex, 1);
+      }
+
+      // Add a new rule for CSS files
+      config.module.rules.push({
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        include: path.resolve(__dirname, '../'),
+      });
+    }
     return config;
   },
 };
