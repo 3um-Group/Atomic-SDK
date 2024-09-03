6a422ba1ba0a4fa32dff60ab1239d64c89d06f64
Bill Tallitsch <william.t@3umgroup.com>
2024-09-03 14:19:22 -0400

    Adding Sidebar menu component

---
+++ src/components/molecules/sidebar/Sidebar.stories.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import Sidebar, { SidebarProps } from './Sidebar';
+import { Button } from '../../atoms/button';
+
+const meta: Meta<typeof Sidebar> = {
+    title: '@3UM-SDK/Sidebar',
+    component: Sidebar,
+    tags: ['autodocs'],
+    argTypes: {
+        children: {
+            control: 'text',
+            description: 'Content displayed inside the modal',
+        },
+        isOpen: {
+            control: 'boolean',
+            description: 'Controls whether the modal is open or closed',
+        },
+        onClose: {
+            action: 'closed',
+            description: 'Function called when the modal requests to be closed',
+        },
+    }
+}
+
+export default meta;
+
+const Template: StoryFn<SidebarProps> = (args) => {
+    const [isOpen, setIsOpen] = useState(args.isOpen);
+  
+    const handleClose = () => setIsOpen(false);
+    const handleOpen = () => setIsOpen(true);
+  
+    return (
+      <>
+        <Button onClick={handleOpen} >
+          Open Modal
+        </Button>
+        <Sidebar {...args} isOpen={isOpen} onClose={handleClose} />
+      </>
+    );
+  };
+
+export const Default = Template.bind({})
+Default.args = {
+    children: 'Dialog content goes here'
+}
+
+export const SaveCancel = Template.bind({});
+SaveCancel.args = {
+  children: 'Do you want to save the changes?',
+  actions: (
+    <>
+      <Button  onClick={() => alert('Save clicked')}>
+        Save
+      </Button>
+      <Button  className= "btn btn-outline btn-error" onClick={() => alert('Cancel clicked')}>
+        Cancel
+      </Button>
+    </>
+  ),
+};
+
+export const YesNo = Template.bind({});
+YesNo.args = {
+    children: 'Are you sure you want to proceed?',
+  actions: (
+    <>
+      <Button  onClick={() => alert('Yes clicked')}>
+        Yes
+      </Button>
+      <Button  className= "btn btn-outline btn-error" onClick={() => alert('No clicked')}>
+        No
+      </Button>
+    </>
+  ),
+};
+
+
+export const Ok = Template.bind({});
+Ok.args = {
+  children: 'Operation completed successfully.',
+  actions: (
+    <Button className="btn btn-outline btn-info" onClick={() => alert('Ok clicked')}>
+        Ok
+      </Button>
+  ),
+};
+
+
