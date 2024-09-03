726919692778bec5c85a95f990fcd47e3424f1a6
Bill Tallitsch <william.t@3umgroup.com>
2024-09-03 16:19:14 -0400

    code cleanup

--- src/components/molecules/sidebar/Sidebar.stories.tsx
+++ src/components/molecules/sidebar/Sidebar.stories.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import Sidebar, { SidebarProps } from './Sidebar';
-import { Button } from '../../atoms/button';
 
 const meta: Meta<typeof Sidebar> = {
     title: '@3UM-SDK/Sidebar',
@@ -11,49 +10,23 @@ const meta: Meta<typeof Sidebar> = {
         children: {
             control: 'text',
             description: 'Content displayed inside the sidebar',
-        },
-        isOpen: {
-            control: 'boolean',
-            description: 'Controls whether the sidebar is open or closed',
-        },
-        onClose: {
-            action: 'closed',
-            description: 'Function called when the sidebar requests to be closed',
-        },
+        }
     }
 }
 
 export default meta;
 
 const Template: StoryFn<SidebarProps> = (args) => {
-    const [isOpen, setIsOpen] = useState(args.isOpen);
-  
-    const handleClose = () => setIsOpen(false);
-    const handleOpen = () => setIsOpen(true);
   
     return (
       <>
-        <Sidebar {...args} isOpen={isOpen} onClose={handleClose} />
+        <Sidebar {...args} />
       </>
     );
   };
 
 export const Default = Template.bind({})
 Default.args = {
-  children: (
-    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-      <li><a>Sidebar Item 1</a></li><li><a>Sidebar Item 2</a></li>
-    </ul>
-  )
+  children: 'Sidebar content goes here'
 }
-
-export const OpenDrawer = Template.bind({});
-OpenDrawer.args = {
-  isOpen: true,
-};
-
-export const ClosedDrawer = Template.bind({});
-ClosedDrawer.args = {
-  isOpen: false,
-};
 
--- src/components/molecules/sidebar/Sidebar.tsx
+++ src/components/molecules/sidebar/Sidebar.tsx
@@ -1,25 +1,27 @@
 import React, { useEffect } from 'react';
 
 export interface SidebarProps {
-    isOpen: boolean;
     children: React.ReactNode;
-    onClose: () => void;
     actions?: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ isOpen = false, onClose, children, actions }) => {
+const Sidebar: React.FC<SidebarProps> = ({ children, actions }) => {
 
     return (
-        
         <div className="drawer">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-            {/* Page content here */}
+                {/* Page content here */}
                 <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open Sidebar</label>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                {children}
+                {}
+                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
+                    <li><a>Sidebar Item 1</a></li>
+                    <li><a>Sidebar Item 2</a></li>
+                </ul>
             </div>
         </div>
     );
