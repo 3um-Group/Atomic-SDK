956d0fa38403af7499db8c50c6cd687e442450eb
Bill Tallitsch <william.t@3umgroup.com>
2024-09-04 10:59:17 -0400

    icon update

--- src/components/molecules/sidebar/Sidebar.tsx
+++ src/components/molecules/sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 export interface SidebarProps {
     children: React.ReactNode;
@@ -12,7 +14,10 @@ const Sidebar: React.FC<SidebarProps> = ({ children, actions }) => {
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 {/* Page content here */}
-                <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open Sidebar</label>
+                {/* <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like"></button> */}
+                <label htmlFor="my-drawer" className="drawer-button">
+                    <FontAwesomeIcon icon={faGear} />
+                </label>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
