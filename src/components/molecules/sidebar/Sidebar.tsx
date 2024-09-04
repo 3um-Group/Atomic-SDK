f61f6bad5c3ea22b956f14229b9348c5016a4c3a
Bill Tallitsch <william.t@3umgroup.com>
2024-09-04 11:57:26 -0400

    added children props

--- src/components/molecules/sidebar/Sidebar.tsx
+++ src/components/molecules/sidebar/Sidebar.tsx
@@ -13,8 +13,6 @@ const Sidebar: React.FC<SidebarProps> = ({ children, actions }) => {
         <div className="drawer">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-                {/* Page content here */}
-                {/* <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like"></button> */}
                 <label htmlFor="my-drawer" className="drawer-button">
                     <FontAwesomeIcon icon={faGear} />
                 </label>
@@ -23,9 +21,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children, actions }) => {
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                 {}
                 <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                    {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    {children}
                 </ul>
             </div>
         </div>
