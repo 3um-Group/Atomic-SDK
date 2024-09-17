ab969b637aee0a17e129617fdb9697c46c130e91
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2024-09-17 12:04:01 -0400

    formatting

--- src/components/molecules/sidebar/Sidebar.stories.tsx
+++ src/components/molecules/sidebar/Sidebar.stories.tsx
@@ -3,27 +3,27 @@ import { Meta, StoryFn } from '@storybook/react';
 import Sidebar, { SidebarProps } from './Sidebar';
 
 const meta: Meta<typeof Sidebar> = {
-    title: '@3UM-SDK/Sidebar',
-    component: Sidebar,
-    tags: ['autodocs'],
-    argTypes: {
-      children: {
-        control: 'text',
-        description: 'Content displayed inside the sidebar',
-      }
+  title: '@3UM-SDK/Sidebar',
+  component: Sidebar,
+  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: 'text',
+      description: 'Content displayed inside the sidebar',
     }
+  }
 }
 
 export default meta;
 
 const Template: StoryFn<SidebarProps> = (args) => {
   
-    return (
-      <React.Fragment>
-        <Sidebar {...args} />
-      </React.Fragment>
-    );
-  };
+  return (
+    <React.Fragment>
+      <Sidebar {...args} />
+    </React.Fragment>
+  );
+};
 
 export const Default = Template.bind({})
 Default.args = {
