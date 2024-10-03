006ace1ee61be89bcae63d9fe8007f0bece741dd
silpa3um <silpa.g@3umgroup.com>
2024-10-03 16:24:51 -0500

    updated to line chart (#88)
    * Updated Version
    
    * updated version
    
    * updated version
    
    * update version
    
    * added classname prop to marketpricechart
    
    * updated version
    
    * used line charts
    
    * updated to line chart

--- src/components/molecules/market-pricechart/MarketPricechart.stories.tsx
+++ src/components/molecules/market-pricechart/MarketPricechart.stories.tsx
@@ -20,9 +20,11 @@ export const Default = Template.bind({});
 Default.args = {
   data: [
 
-    { date: '2024-07-15', volume: 0.010, averagePrice: 0.008 },
+    { date: '2024-07-15', volume: 0.010, averagePrice: 0.011 },
     { date: '2024-08-14', volume: 0.012, averagePrice: 0.011 },
-    { date: '2024-09-10', volume: 0.017, averagePrice: 0.012 },
+    { date: '2024-09-10', volume: 0.015, averagePrice: 0.012 },
+    { date: '2024-10-14', volume: 0.018, averagePrice: 0.015 },
+    { date: '2024-11-10', volume: 0.020, averagePrice: 0.018 },
   ],
   className: 'bg-gray-100 p-4 rounded-lg',
 };
